efe30e25612ed5008a417391843bc2d4f372a0cd
Ahasan Habib Sany <sany.baust@gmail.com>
2022-04-17 16:02:18 +0600

    structure done

--- photography/src/App.js
+++ photography/src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+
 function App() {
-	return (
-		<div style={{ textAlign: 'center' }}>
-			<h1>Welcome</h1>
-		</div>
-	)
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
