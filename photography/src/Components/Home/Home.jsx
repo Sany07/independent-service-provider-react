e307b4bc04d9ad5c95fcb286ca503d362ae26fc0
Ahasan Habib Sany <sany.baust@gmail.com>
2022-04-18 13:38:20 +0600

    checkout done

--- photography/src/Components/Home/Home.jsx
+++ photography/src/Components/Home/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
         <div className="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Before they sold out
+              I'm H SANY
               <br className="hidden lg:inline-block" />
-              readymade gluten
+              Wedding Photographer
             </h1>
             <p className="mb-8 leading-relaxed">
               Copper mug try-hard pitchfork pour-over freegan heirloom neutra
@@ -31,7 +31,7 @@ const Home = () => {
             <img
               className="object-cover object-center rounded"
               alt="hero"
-              src="https://dummyimage.com/720x600"
+              src="https://demos.elemisthemes.com/brailie/style/images/art/f7.jpg"
             />
           </div>
         </div>
--- photography/src/Components/Services/Services.jsx
+++ photography/src/Components/Services/Services.jsx
@@ -8,12 +8,12 @@ const Services = () => {
         <div className="container px-5 py-24 mx-auto">
           <div className="text-center mb-20">
             <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">
-              My Specialties
+              The Standard
             </h1>
             <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
-              I'd like to give you a unique photography experience, built to
-              serve you best and capture your most important day creatively and
-              beautifully.
+              Awesome deals with simple pricing model are here! <br />
+              We offer great prices, premium products and quality service for
+              your business..
             </p>
             <div className="flex mt-6 justify-center">
               <div className="w-16 h-1 rounded-full bg-pink-500 inline-flex" />
@@ -29,14 +29,16 @@ const Services = () => {
                 />
                 <div className="p-6">
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    Wedding Photography
+                    The Standard
                   </h1>
                   <p className="leading-relaxed mb-3">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
+                    Photographer on site for 4 hours of event coverage <br />
+                    600+ edited, hi-res images
+                    <br />
+                    Custom Services <br /> 30 days Delevery
                   </p>
                   <div className="flex items-center flex-wrap ">
-                    <Link to="/checkout/jf">
+                    <Link to="/checkout/p-1">
                       <button
                         className=" inline-flex items-center md:mb-2 lg:mb-0
                     text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg
@@ -57,35 +59,9 @@ const Services = () => {
                         </svg>
                       </button>
                     </Link>
-                    <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                      <svg
-                        className="w-4 h-4 mr-1"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                        <circle cx={12} cy={12} r={3} />
-                      </svg>
-                      1.2K
-                    </span>
-                    <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-                      <svg
-                        className="w-4 h-4 mr-1"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                      </svg>
-                      6
-                    </span>
+                    <h3 className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-lg pl-3 py-1 border-l-2 border-gray-200">
+                      545$
+                    </h3>
                   </div>
                 </div>
               </div>
@@ -99,14 +75,16 @@ const Services = () => {
                 />
                 <div className="p-6">
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    The 400 Blows
+                    The Advanced
                   </h1>
                   <p className="leading-relaxed mb-3">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
+                    Photographer on site for 10 hours of event coverage <br />{" "}
+                    550+ edited, hi-res images
+                    <br />
+                    Custom Services <br /> 20 days Delevery
                   </p>
                   <div className="flex items-center flex-wrap">
-                    <Link to="/checkout/jf">
+                    <Link to="/checkout/p-2">
                       <button
                         className=" inline-flex items-center md:mb-2 lg:mb-0
                     text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg
@@ -127,35 +105,9 @@ const Services = () => {
                         </svg>
                       </button>
                     </Link>
-                    <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                      <svg
-                        className="w-4 h-4 mr-1"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                        <circle cx={12} cy={12} r={3} />
-                      </svg>
-                      1.2K
-                    </span>
-                    <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-                      <svg
-                        className="w-4 h-4 mr-1"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                      </svg>
-                      6
-                    </span>
+                    <h3 className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-lg pl-3 py-1 border-l-2 border-gray-200">
+                      745$
+                    </h3>
                   </div>
                 </div>
               </div>
@@ -169,14 +121,17 @@ const Services = () => {
                 />
                 <div className="p-6">
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                  Sports Photographe
+                    The Ultimate
                   </h1>
                   <p className="leading-relaxed mb-3">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
+                    Photographer on site for 10 hours of event coverage <br />{" "}
+                    750+ edited, hi-res images
+                    <br />
+                    Custom Services
+                    <br /> 10 days Delevery
                   </p>
                   <div className="flex items-center flex-wrap ">
-                    <Link to="/checkout/jf">
+                    <Link to="/checkout/p-3">
                       <button
                         className=" inline-flex items-center md:mb-2 lg:mb-0
                     text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg
@@ -197,35 +152,9 @@ const Services = () => {
                         </svg>
                       </button>
                     </Link>
-                    <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                      <svg
-                        className="w-4 h-4 mr-1"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                        <circle cx={12} cy={12} r={3} />
-                      </svg>
-                      1.2K
-                    </span>
-                    <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-                      <svg
-                        className="w-4 h-4 mr-1"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                      </svg>
-                      6
-                    </span>
+                    <h3 className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-lg pl-3 py-1 border-l-2 border-gray-200">
+                      1145$
+                    </h3>
                   </div>
                 </div>
               </div>
